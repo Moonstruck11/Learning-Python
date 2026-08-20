b4cf522a848e9f69ad27a4daf0800fac8c8924bf
Moonstruck11 <moonwalks888@gmail.com>
2026-08-20 10:31:00 +0800

    Add files via upload

--- grade_calculator.py
+++ grade_calculator.py
@@ -1,28 +1,28 @@
-name=input('What is your name?')
-scr=input('What is your score?')
-try:
-    scr=int(scr)
-except:
-    print('Invalid input')
-else:
-    if 0<=scr<=100:
-        if scr>=80:
-            grade = 'A'
-            print(name,', your grade is', grade)
-        elif scr >= 70:
-            grade = 'B'
-            print(name,', your grade is', grade)
-        elif scr>=60:
-            grade = 'C'
-            print(name,', your grade is', grade)
-        elif scr>=50:
-            grade = 'D'
-            print(name,', your grade is', grade)
-        else:
-            grade = 'F'
-            print(name,', your grade is', grade)
-        if grade!='F':
-            print('You passed!')
-        else:
-            print('You failed.')
+import random
+num = random.randint(1,100)
+attempts = 0
+while num>0:
+    try:
+        guess = int(input('Guess a number between 1 and 100'))
+        if guess<1 or guess>100:
+            print('Please enter a number between 1 and 100')
+            continue
+    except:
+        print('Invalid input')
+        continue
+    if guess > num:
+        attempts = attempts + 1
+        print('Too high!')
+        continue
+    if guess < num:
+        attempts = attempts + 1
+        print('Too low!')
+    if guess == num:
+        attempts = attempts + 1
+        print(('Correct!'))
+        print('You got it in', attempts, 'attempts')
+        break
+
+
+
 
