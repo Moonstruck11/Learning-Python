73db59e8ff5363d9a342be474fe0d3d73b3ba050
Francis Luca Moon <moonwalks888@gmail.com>
2026-08-26 10:48:31 +0800

    Add files via upload

---
+++ function_sum.py
@@ -0,0 +1,12 @@
+def add(a, b):
+    return a + b
+first = int(input('Enter first number: '))
+second = int(input('Enter first number: '))
+result = add(first, second)
+print('The sum is', result)
+
+
+
+
+
+
