6c532b4b5d9afc3dfa9857a2934eac41df09fb0f
Francis Luca Moon <moonwalks888@gmail.com>
2026-08-26 11:05:34 +0800

    Add files via upload

---
+++ function_calculate_grade.py
@@ -0,0 +1,21 @@
+def calculate_grade(score):
+    if score >= 90:
+        return 'A'
+    elif score >= 80:
+        return 'B'
+    elif score >= 70:
+        return 'C'
+    elif score >= 60:
+        return 'D'
+    else:
+        return 'F'
+score = int(input('Enter your score: '))
+result = calculate_grade(score)
+print(result)
+
+
+
+
+
+
+
