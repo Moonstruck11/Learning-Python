ab3bd26866ddf8f0c3794fd2ccb3c249e501a68a
Francis Luca Moon <moonwalks888@gmail.com>
2026-08-28 14:19:49 +0800

    Add files via upload

---
+++ function_numbers_squared.py
@@ -0,0 +1,26 @@
+def square_numbers(numbers):
+    numbers_squared = []
+    for number in numbers:
+        number = number * number
+        numbers_squared.append(number)
+    return numbers_squared
+numbers = [2, 4, 6, 8]
+result = square_numbers(numbers)
+print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
